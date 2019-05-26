6918037448b9dcef27c1875085e0abce62b7e2de
Carlos <crosales65@gmail.com>
2019-05-26 18:37:31 -0400

    Initial Commit

---
+++ RandomNumbers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomNumbers
+//
+//  Created by Carlos on 5/26/19.
+//  Copyright © 2019 oSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
