0c845d60b9a1fcfadff7b4313546f769ef7f48b4
Carlos <crosales65@gmail.com>
2019-05-30 11:55:54 -0400

    App sin funciones random

--- RandomNumbers/ViewController.swift
+++ RandomNumbers/ViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var tituloApp: UILabel!
+    @IBOutlet weak var opcionesSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var reiniciarButton: UIButton!
+    @IBOutlet weak var contadorLabel: UILabel!
+    @IBOutlet weak var historiaLabel: UILabel!
+    @IBOutlet weak var numeroButton: UIButton!
+    
+    // MARK: - Constantes
+    
+    private let simboloSinNumero = "🎲"
+    
+    // MARK: - Variables
+    
+    private var numeroMin: Int = 0
+    private var numeroMax: Int = 1
+    private var numeroActual: Int = -1
+    private var contador: Int = 0
+    private var historial: String = ""
+    
+    // MARK: - Acciones
+    
+    @IBAction func reiniciarButtonAction(_ sender: UIButton) {
+        reiniciar()
+    }
+    
+    @IBAction func numeroButtonAction(_ sender: UIButton) {
+        generarNumero()
+    }
+    
+    // MARK: - Métodos
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        reiniciar()
     }
 
+    func reiniciar() {
+        numeroActual = -1
+        historial = ""
+        contador = 0
+        contadorLabel.text = ""
+        historiaLabel.text = ""
+        numeroButton.setTitle(simboloSinNumero, for: .normal)
+    }
 
+    func generarNumero() {
+        // lógica para generar un número aleatorio entre numeroMin y numeroMax
+        numeroActual = numeroMax   // mientras
+
+        // actualizar el contador
+        contador += 1
+        // actualizar el historial
+        historial += " " + String(numeroActual)
+        // desplegar datos
+        contadorLabel.text = String(contador)
+        historiaLabel.text = historial
+        numeroButton.setTitle(String(numeroActual), for: .normal)
+    }
 }
 
